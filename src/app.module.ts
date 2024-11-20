f03b238a6f022886d74a01ee52a95e435dacc9f9
freddy <freddy.le.goldenowl@gmail.com>
2024-11-20 16:59:48 +0700

    feat: add environment configuration and update database connection settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: 'Freddy.le123',
-      username: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_DATABASE,
       entities: [],
-      database: 'postgres',
       synchronize: true,
     }),
   ],
