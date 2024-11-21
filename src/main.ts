155e034f9771c1f9329b57fbd8b53536d466fe44
freddy <freddy.le.goldenowl@gmail.com>
2024-11-21 12:18:43 +0700

    feat: enhance UserLoginDTO with email validation and enable global validation in main application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './interceptors';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable global validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Automatically transform plain objects to DTO instances
+      whitelist: true, // Remove properties that don't exist in the DTO
+    }),
+  );
   app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(process.env.PORT ?? 3001);
